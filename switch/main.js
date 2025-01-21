0dc165a8408da72bedb6983827ccfa3cb5a716d2
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2025-01-21 11:03:17 +0900

    Added switch-index

--- switch/main.js
+++ switch/main.js
@@ -1,34 +1,44 @@
-const buttonThemeLight = document.querySelector('.theme-button-light');
-const buttonThemeDark = document.querySelector('.theme-button-dark');
 
-const buttonCardViewTile = document.querySelector('.card-view-button-tile');
-const buttonCardViewStandard = document.querySelector('.card-view-button-standard');
-const buttonCardViewCompact = document.querySelector('.card-view-button-compact');
 
-const cards = document.querySelector('.cards');
 
-buttonThemeLight.addEventListener('click', () => {
-  document.documentElement.dataset['themeName'] = 'light'
-});
 
-buttonThemeDark.addEventListener('click', () => {
-  document.documentElement.dataset['themeName'] = 'dark'
-});
 
-buttonCardViewTile.addEventListener('click', () => {
-  cards.classList.remove('standard')
-  cards.classList.remove('compact')
-  cards.classList.add('tile')
-});
 
-buttonCardViewStandard.addEventListener('click', () => {
-  cards.classList.remove('compact')
-  cards.classList.remove('tile')
-  cards.classList.add('standard')
-});
 
-buttonCardViewCompact.addEventListener('click', () => {
-  cards.classList.remove('tile')
-  cards.classList.remove('standard')
-  cards.classList.add('compact')
-});
+
+
+
+// const buttonThemeLight = document.querySelector('.theme-button-light');
+// const buttonThemeDark = document.querySelector('.theme-button-dark');
+
+// const buttonCardViewTile = document.querySelector('.card-view-button-tile');
+// const buttonCardViewStandard = document.querySelector('.card-view-button-standard');
+// const buttonCardViewCompact = document.querySelector('.card-view-button-compact');
+
+// const cards = document.querySelector('.cards');
+
+// buttonThemeLight.addEventListener('click', () => {
+//   document.documentElement.dataset['themeName'] = 'light'
+// });
+
+// buttonThemeDark.addEventListener('click', () => {
+//   document.documentElement.dataset['themeName'] = 'dark'
+// });
+
+// buttonCardViewTile.addEventListener('click', () => {
+//   cards.classList.remove('standard')
+//   cards.classList.remove('compact')
+//   cards.classList.add('tile')
+// });
+
+// buttonCardViewStandard.addEventListener('click', () => {
+//   cards.classList.remove('compact')
+//   cards.classList.remove('tile')
+//   cards.classList.add('standard')
+// });
+
+// buttonCardViewCompact.addEventListener('click', () => {
+//   cards.classList.remove('tile')
+//   cards.classList.remove('standard')
+//   cards.classList.add('compact')
+// });
